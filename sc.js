8e5bcc6fb34647129404cbd5d19e0727e30a54bd
prodiGamer1000 <168657105+prodiGamer1000@users.noreply.github.com>
2024-11-22 11:54:56 -0300

    index.html

---
+++ sc.js
@@ -0,0 +1,5 @@
+const sv = document.querySelector('.sv')
+const a = document.querySelector('.s')
+sv.addEventListener('click', ()=>{
+    a.innerHTML = "<ul><li>item 1</li><li>item 2</li><li>item 3</li></ul>"
+})
